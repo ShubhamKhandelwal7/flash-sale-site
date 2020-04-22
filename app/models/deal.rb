02ad617e373a0eb678b84ef7ef210081aafe839c
akhil <akhil@vinsol.com>
2020-04-22 14:08:50 +0530

    Added comments

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,3 +1,4 @@
+#FIXME_AB: move this validator to app/validators folder
 class PriceValidator < ActiveModel::Validator
   def validate(record)
     return true if record.price && record.price > (record.discount_price || 0)
@@ -5,6 +6,7 @@ class PriceValidator < ActiveModel::Validator
   end
 end
 
+#FIXME_AB: move this to app/exceptions
 class UnPublishabileError < StandardError
 end
 
@@ -13,6 +15,7 @@ class Deal < ApplicationRecord
   acts_as_paranoid
   has_many_attached :images, dependent: :purge_later
 
+  #FIXME_AB: let's add a validation to qty to be > 0 when published_at is present. if not scheduled to publish, we can leave qty validation
   validates :title, :description, :quantity, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :title, uniqueness: true, case_sensitive: false, if: -> { title.present? }
@@ -24,6 +27,7 @@ class Deal < ApplicationRecord
   scope :published_on, ->(date) { where(published_at: date.beginning_of_day..date.end_of_day) }
 
   def can_be_scheduled_to_publish_on(date)
+    #FIXME_AB: need to consider current deal
     valid? && (self.class.published_deals_on(date) < DEALS[:max_deals_per_day].to_i)
   end
 
@@ -32,15 +36,19 @@ class Deal < ApplicationRecord
   end
 
   def cover_image
-    images.first  
+    images.first
   end
 
   private def ensure_min_image_upload
-    return if images.count >= DEALS[:min_images_limit].to_i
-    errors.add(:images, "#{I18n.t("errors.image")} #{DEALS[:min_images_limit]}")
+    if images.count < DEALS[:min_images_limit].to_i
+      errors.add(:images, "#{I18n.t("errors.image")} #{DEALS[:min_images_limit]}")
+    end
   end
 
   private def ensure_publishability_criteria
+    #FIXME_AB: in case of updating the deal which is scheduled to publish on specific day, it will fail
+    #FIXME_AB: make a method for every condition can_be_scheduled_to_publish_on?(date) && valid_qty_available? && valid_publish_date_margin
+
     return if self.class.published_deals_on(published_at) <= (DEALS[:max_deals_per_day].to_i - 1) &&
               quantity >= DEALS[:min_quant_limit].to_i &&
               published_at > (Date.today + 1.day)
