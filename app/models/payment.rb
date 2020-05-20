e09fbd8be4e34df30b8be763fab4fac80062d6f5
Shubham369 <shubham.khandelwal@vinsol.com>
2020-05-20 14:32:39 +0530

    PR changes

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -14,7 +14,7 @@ class Payment < ApplicationRecord
   scope :no_success, -> { where.not(state: :succeeded) }
   
   def stripe_transact(token)
-    if (stripe_customer = create_stripe_customer) && add_source(stripe_customer.id, token) && (charge = create_charge(stripe_customer.id))
+    if (stripe_customer = get_stripe_customer) && add_source(stripe_customer.id, token) && (charge = create_charge(stripe_customer.id))
       update_payment(charge)
       save && succeeded?
     else
@@ -24,9 +24,17 @@ class Payment < ApplicationRecord
     false
   end
 
+  private def get_stripe_customer
+    if order.user.stripe_customer_id.blank? && !create_stripe_customer
+      return false
+    end
+    Stripe::Customer.retrieve(order.user.stripe_customer_id)
+  end
+
   private def create_stripe_customer
-    Stripe::Customer.create({
+    new_stripe_cus = Stripe::Customer.create({
       name: order.user.name,
+      email: order.user.email,
       address: {
         line1: order.address.home_address,
         postal_code: order.address.pincode,
@@ -35,6 +43,7 @@ class Payment < ApplicationRecord
         country: order.address.country.upcase,
       }
     })
+    new_stripe_cus && order.user.update(stripe_customer_id: new_stripe_cus.id)
   rescue StandardError
     false
   end
@@ -53,7 +62,7 @@ class Payment < ApplicationRecord
       Stripe::Charge.create({
         amount: order.total_amount.round * 100,
         currency: 'inr',
-        description: 'Order Payment',
+        description: "Payment for Order ID: #{order.id}",
         customer: customer_id,
       })
     end
@@ -62,12 +71,20 @@ class Payment < ApplicationRecord
   end
 
   private def update_payment(stripe_resp)
+    self.stripe_response = stripe_resp
     self.transaction_id = stripe_resp.id
     self.state = stripe_resp.status
     self.method = stripe_resp.payment_method_details['type']
-    self.amount = stripe_resp.amount
+    self.amount = stripe_resp.amount / 100
     self.category = stripe_resp.object
     self.currency = stripe_resp.currency
+    if self.method == 'card'
+      card = stripe_resp.payment_method_details['card']
+      self.card_brand = card['brand']
+      self.card_last_digits = card['last4']
+      self.card_exp_month = card['exp_month']
+      self.card_exp_year = card['exp_year']
+    end
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_180225) do
+ActiveRecord::Schema.define(version: 2020_05_20_082102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -117,6 +117,11 @@ ActiveRecord::Schema.define(version: 2020_05_19_180225) do
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.jsonb "stripe_response"
+    t.integer "card_last_digits"
+    t.integer "card_exp_year"
+    t.integer "card_exp_month"
+    t.string "card_brand"
     t.index ["order_id"], name: "index_payments_on_order_id"
     t.index ["transaction_id"], name: "index_payments_on_transaction_id", unique: true
   end
@@ -134,6 +139,7 @@ ActiveRecord::Schema.define(version: 2020_05_19_180225) do
     t.datetime "verification_token_sent_at"
     t.datetime "verified_at"
     t.boolean "admin", default: false, null: false
+    t.string "stripe_customer_id"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["password_reset_token"], name: "index_users_on_password_reset_token", unique: true
